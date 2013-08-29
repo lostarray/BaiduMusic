3ba98ccc77f263db0d1cadf2a79a0e8d61fabc50
Xin Chen <dev@lostarray.com>
2013-08-29 22:37:48 +0800

    搜索结果显示匹配的歌词 增加错误提示页面

--- baidumusic/views.py
+++ baidumusic/views.py
@@ -1,11 +1,12 @@
 import json
 import re
 import urllib2
-from pyquery import PyQuery as pq
+from pyquery import PyQuery
 
 from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response
 
+search_prefix = 'http://music.baidu.com/search?key='
 song_query = 'http://music.baidu.com/data/music/fmlink?songIds={}&type={}&rate={}'
 pattern = re.compile(r'(http://)?music.baidu.com/song/(?P<song_id>\d*)')
 
@@ -35,7 +36,11 @@ def song(request, song_id):
             }
         except:
             song_dict[key] = None
-    return render_to_response('song.html', song_dict)
+
+    if 'song_name' in song_dict:
+        return render_to_response('song.html', song_dict)
+    else:
+        return render_to_response('error.html')
 
 
 def search(request):
@@ -51,20 +56,24 @@ def search(request):
         return HttpResponseRedirect('/song/' + input_content)
 
     else:
-        url = 'http://music.baidu.com/search?key=' + input_content.replace(' ', '+').encode('utf-8')
+        url = search_prefix + input_content.replace(' ', '+').encode('utf-8')
         page = urllib2.urlopen(url).read()
-        d = pq(page.decode('utf-8'))
+        d = PyQuery(page.decode('utf-8'))
         song_list = d('li.song-item-hook')
+        if not song_list:
+            return render_to_response('error.html')
         result = []
 
         for song in song_list:
-            song = pq(song)
-            song_info = json.loads(song.attr('data-songitem'))['songItem']
-            song_dict = {}
+            song = PyQuery(song)
             if not song('span.icon-thirdparty'):
+                song_dict = {}
+                song_info = json.loads(song.attr('data-songitem'))['songItem']
                 song_dict['link'] = '/song/' + str(song_info['sid'])
                 song_dict['song_name'] = song_info['sname']
                 song_dict['artist_name'] = song_info['author']
                 song_dict['album_name'] = song('span.album-title > a').html()
+                lyric = song('div.lyric-item').html()
+                song_dict['lyric'] = lyric.strip().replace('\n', ' ') if lyric else None
                 result.append(song_dict)
         return render_to_response('search.html', {'result': result})
